a8d8d85f8eaefed03b4a376871317ce4fff58aac
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-06-25 11:38:30 +0200

    Ajout de commentaire

--- questions.js
+++ questions.js
@@ -3,41 +3,41 @@
  * Trouvez la façon de faire la plus optimal.
  * Il peut y avoir plusieur façon de faire.
  */
-var tailleString = function (texte) {
+var tailleString = function (texte) {//Donne le nombre de caractère de la chaine de caractère//
     return texte.length;
 }
-var remplaceECar = function (texte) {
+var remplaceECar = function (texte) {//Remplace le premier caractère spécifié par un autre//
   return texte.replace('e',' ');
 }
-var concatString = function (texte1, texte2) {
+var concatString = function (texte1, texte2) {//Concatène deux chaines de caractères//
     return texte1.concat(texte2);
 }
-var afficherCar5 = function (texte) {
+var afficherCar5 = function (texte) {//Affiche le caractère à la 5ème position//
     return texte.charAt(6);
 }
-var afficher9Car = function (texte) {
+var afficher9Car = function (texte) {//Affiche les 9 premiers caractères//
     return texte.substr(0, 9);
 }
-var majusculeString = function (texte) {
+var majusculeString = function (texte) {//Affiche la chaine de caractère tout en majuscule//
     return texte.toUpperCase();
 }
-var minusculeString = function (texte) {
+var minusculeString = function (texte) {//Affiche la chaine de caractère tout en minuscule//
     return texte.toLowerCase();
 }
-var SupprEspaceString = function (texte) {
+var SupprEspaceString = function (texte) {//Supprime les espaces d'une chaine de caractères//
     return texte.trim();
 }
-var IsString = function (texte) {
+var IsString = function (texte) {//Vérifie l'existence d'une chaine de caractère//
     return typeof texte === 'string';
 }
-var AfficherExtensionString = function (texte) {
+var AfficherExtensionString = function (texte) {//Affiche l'exention d'un fichier type image ou autre//
     var x = texte.split('.');
     return x[1];
 }
-var NombreEspaceString = function (texte) {
+var NombreEspaceString = function (texte) {//Compte le nombre d'espace dans une chaine de caractère//
     return texte.match(/[' ']+/g).length;
 }
-var InverseString = function (texte) {
+var InverseString = function (texte) {//Inverse tous les caractères d'une chaine de caractères//
     var array = texte.split('');
     array.reverse();
     texte = array.join('');
@@ -47,24 +47,24 @@ var InverseString = function (texte) {
 /**
  * Exercices sur les nombres et les caluls mathématiques
  */
-var calculPuissance = function (x, y) {
+var calculPuissance = function (x, y) {//Calcul de puissance//
     return Math.pow(x, y);
 }
-var valeurAbsolue = function (nombre) {
+var valeurAbsolue = function (nombre) {//Met un nombre en valeur absolue//
     return Math.abs(nombre);
 }
-var valeurAbsolueArray = function (array) {
+var valeurAbsolueArray = function (array) {//Met tout les nombre d'un tableau en valeur absolue//
   for (var i = 0; i < array.length; i++){
   array[i] = Math.abs(array[i]);
   }
   return array;
 }
-var sufaceCercle = function (rayon) {
+var sufaceCercle = function (rayon) {//Calcul de la surface d'un cercle//
     return Math.ceil((rayon*rayon) * 3.14);
 }
-var hypothenuse = function (ab, ac) {
+var hypothenuse = function (ab, ac) {//Calcul de l'hypoténuse//
     return Math.sqrt((ab*ab)+(ac*ac));
 }
-var calculIMC = function (poids, taille) {
+var calculIMC = function (poids, taille) {//Calcul de l'IMC//
   return Math.round((poids/(taille*taille))*100)/100;
 }
